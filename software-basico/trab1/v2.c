6886b36119c9da39b1bf319f05782655a621f1f7
Rafael Santos <rafa1405rafael@gmail.com>
2023-11-10 10:19:33 -0300

    .

--- software-basico/trab1/v2.c
+++ software-basico/trab1/v2.c
@@ -18,6 +18,7 @@ void *alocaMem (long int num_bytes) {
     long int *tamanho;
     void *bloco;
     void *atualHeap;
+    long int dif;
 
     /* se lista vazia */
     if (inicioHeap == topoHeap) {
@@ -33,16 +34,18 @@ void *alocaMem (long int num_bytes) {
         *tamanho = num_bytes;
         
         /* coloco o endereço do início do bloco */
-        bloco = inicioHeap + 16;
+        /* SERA Q ISSO EH NECESSARIO */
+        // bloco = inicioHeap + 16;
 
         /* atualiza topo da heap */
         topoHeap = sbrk(0);
 
-        return bloco;
+        return inicioHeap;
     }
     
     atualHeap = inicioHeap;
 
+    /* aloca em espaço vazio */
     while (atualHeap != topoHeap) {
         if (*((long int*) (atualHeap)) != 1) {
             if (*((long int*) (atualHeap + 8)) >= num_bytes) {
@@ -51,14 +54,39 @@ void *alocaMem (long int num_bytes) {
                 *ocupado = 1;
                 
                 /* coloco o endereço do início do bloco */
-                bloco = atualHeap + 16;
+                /* SERA Q ISSO EH NECESSARIO */
+                // bloco = atualHeap + 16;
+                
+                /* dividir um bloco em 2 */
+                dif = *((long int*) (atualHeap + 8)) - num_bytes;
+                if (dif > 16) {
+                    /* atualiza tamanho do bloco */
+                    tamanho = atualHeap + 8;
+                    *tamanho = num_bytes;
 
-                return bloco;
+                    /* novo bloco desocupado */
+                    /* + 16 (informações gerenciais do bloco anterior) + num_bytes (espaço do bloco anterior) */
+                    ocupado = atualHeap + 16 + num_bytes;
+                    *ocupado = 0;
+
+                    /* tamanho do novo bloco */
+                    /* + 24 (informações gerenciais do bloco anterior + ocupado do bloco atual) + num_bytes (espaço do bloco anterior) */
+                    tamanho = atualHeap + 24 + num_bytes;
+                    *tamanho = dif - 16;
+
+                    /* coloco o endereço do início do bloco */
+                    /* SERA Q ISSO EH NECESSARIO */
+                    // bloco = atualHeap + 16;
+                } 
+
+                return atualHeap;
             }
         }
 
         atualHeap = atualHeap + 16 + *((long int*) (atualHeap + 8));
     }
+
+    /* aloca mais espaço na heap */
 
     /* aloca o tamanho do noh */
     brk(atualHeap + (16 + num_bytes));
@@ -72,42 +100,103 @@ void *alocaMem (long int num_bytes) {
     *tamanho = num_bytes;
 
     /* coloco o endereço do início do bloco */
-    bloco = atualHeap + 16;
+    /* SERA Q ISSO EH NECESSARIO */
+    // bloco = atualHeap + 16;
 
     /* atualiza topo da heap */
     topoHeap = sbrk(0);
 
-    return bloco;
+    return atualHeap;
 }
 
 int *liberaMem (void *bloco) {
     long int *ocupado;
 
     /* indica que o bloco está livre */
-    ocupado = bloco - 16;
+    ocupado = bloco;
     *ocupado = 0;
 }
 
+void imprimeMapa () {
+    void *atualHeap;
+
+    atualHeap = inicioHeap;
+
+    while (atualHeap != topoHeap) {
+        printf ("################");
+
+        if (*((long int*) (atualHeap)) == 1) {
+            for (int i = 0; i < *((long int*) (atualHeap + 8)); i++) {
+                printf ("+");
+            }
+        }
+
+        if (*((long int*) (atualHeap)) == 0) {
+            for (int i = 0; i < *((long int*) (atualHeap + 8)); i++) {
+                printf ("-");
+            }
+        }
+        
+        atualHeap = atualHeap + 16 + *((long int*) (atualHeap + 8));
+    }
+    printf ("\n");
+}
+
 int main () {
+    void *a, *b, *c, *d, *e;
+
+    printf ("Manipulando a Heap\n");
+
+    iniciaAlocador ();
+
+    printf ("# INICIO DA HEAP: %p\n", inicioHeap);
+    printf ("# TOPO DA HEAP:   %p\n", topoHeap);
+    printf ("\n");
+
+    printf ("Aloca bloco TAM 50\n");
+    a = alocaMem (50);
+    printf ("# INICIO BLOCO A: %p\n", a);
+    printf ("# TOPO DA HEAP:   %p\n", topoHeap);
+    imprimeMapa ();
     printf ("\n");
     
-    iniciaAlocador ();
-    printf ("# incio: %p\n", inicioHeap);
-    printf ("# topo inicial: %p\n", topoHeap);
+    printf ("Aloca bloco TAM 5\n");
+    b = alocaMem (5);
+    printf ("# INICIO BLOCO B: %p\n", b);
+    printf ("# TOPO DA HEAP:   %p\n", topoHeap);
+    imprimeMapa ();
+    printf ("\n");
 
-    void *bloco = alocaMem(100);
-    printf ("# topo atual: %p\n", topoHeap);
+    printf ("Libera bloco TAM A\n");
+    liberaMem (a);
+    imprimeMapa ();
+    printf ("\n");
 
-    liberaMem (bloco);
+    printf ("Aloca bloco TAM 10\n");
+    c = alocaMem (10);
+    printf ("# INICIO BLOCO C: %p\n", c);
+    printf ("# TOPO DA HEAP:   %p\n", topoHeap);
+    imprimeMapa ();
+    printf ("\n");
+    
+    printf ("Aloca bloco TAM 8\n");
+    d = alocaMem (8);
+    printf ("# INICIO BLOCO D: %p\n", d);
+    printf ("# TOPO DA HEAP:   %p\n", topoHeap);
+    imprimeMapa ();
+    printf ("\n");
 
-    void *bloco2 = alocaMem(101);
-    printf ("# topo atual: %p\n", topoHeap);
+    printf ("Libera bloco TAM D\n");
+    liberaMem (d);
+    imprimeMapa ();
+    printf ("\n");
 
-    void *bloco3 = alocaMem(99);
-    printf ("# topo atual: %p\n", topoHeap);
-
-    void *bloco4 = alocaMem(100);
-    printf ("# topo atual: %p\n", topoHeap);
+    printf ("Aloca bloco TAM 25\n");
+    e = alocaMem (25);
+    printf ("# INICIO BLOCO D: %p\n", e);
+    printf ("# TOPO DA HEAP:   %p\n", topoHeap);
+    imprimeMapa ();
+    printf ("\n");
 
     finalizaAlocador ();
     
