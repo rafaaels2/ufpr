db711a9e3b2e37ef7d04cb07c7cd4bcd22c57a72
Rafael Santos <rafa1405rafael@gmail.com>
2024-04-02 10:53:46 -0300

    .

---
+++ so/p3/ppos_core.c
@@ -0,0 +1,122 @@
+// GRR20211798 Rafael Gonçalves dos Santos
+
+#include <stdio.h>
+#include <ucontext.h>
+#include <stdlib.h>
+#include "ppos.h"
+#include "queue.h"
+
+#define STACKSIZE 64*1024	/* tamanho de pilha das threads */
+
+task_t dispacherTask, mainTask, *currentTask;
+filatask_t *readyQueue;
+int id = 0;
+
+void print_elem (void *ptr)
+{
+   filatask_t *elem = ptr ;
+
+   if (!elem)
+      return ;
+
+   elem->prev ? printf ("%d", elem->prev->task.id) : printf ("*") ;
+   printf ("<%d>", elem->task.id) ;
+   elem->next ? printf ("%d", elem->next->task.id) : printf ("*") ;
+}
+
+void scheduler () {
+    queue_append ((queue_t**) readyQueue, (queue_t*) currentTask);
+
+}
+
+void dispacher (void * arg) {
+    queue_remove ((queue_t**) readyQueue, (queue_t*) &dispacherTask);
+}
+
+void ppos_init () {
+    // id da tarefa main
+    mainTask.id = 0;
+
+    // contexto atual é o contextoMain
+    currentTask = &mainTask;
+
+    task_init (&dispacherTask, dispacher, "Dispacher");
+    
+    // desativa o buffer da saida padrao (stdout), usado pela função printf 
+    setvbuf (stdout, 0, _IONBF, 0);
+
+    #ifdef DEBUG
+    printf ("# sistema inicializado\n") ;
+    #endif
+}
+
+int task_id () {
+    return currentTask -> id;
+}
+
+int task_init (task_t *task, void (*start_func)(void *), void *arg) {
+    // define o id da tarefa
+    task -> id = ++id;
+    task -> prev = NULL;
+    task -> next = NULL;
+
+    // salva o contexto atual na task iniciada
+    if (getcontext (&task -> context) != 0) {
+        perror ("Erro no getcontext: ");
+        return -1;
+    }
+    
+    // aloca memoria para o contexto
+    char *stack = malloc (STACKSIZE);
+
+    if (stack) {
+        // inicializa o contexto
+        task -> context.uc_stack.ss_sp = stack;
+        task -> context.uc_stack.ss_size = STACKSIZE;
+        task -> context.uc_stack.ss_flags = 0;
+        task -> context.uc_link = 0;
+    }
+
+    else {
+        perror ("Erro na criação da pilha: ");
+        return -1;
+    }
+
+    // passa a funcao para a funcao e seus respectivos parametros para a task
+    makecontext (&task -> context, (void*)(*start_func), 1, arg);
+
+    #ifdef DEBUG
+    printf ("# task %d criada por %d\n", id, currentTask -> id) ;
+    #endif
+
+    return id;
+}
+
+void task_exit (int exit_code) {
+    // retorna para a main
+    task_switch (&mainTask);
+}
+
+int task_switch (task_t *task) {
+    // atualiza o contexto atual
+    task_t *aux = currentTask;
+    currentTask = task;
+
+    #ifdef DEBUG
+    printf ("# troca de task %d -> %d\n", aux -> id, currentTask -> id) ;
+    #endif
+
+    // realiza a troca de contexto
+    if (swapcontext (&aux -> context, &task -> context) != 0) {
+        perror ("Erro no swapcontext: ");
+        return -1;
+    }
+
+    return 0;
+}
+
+void task_yield () {
+    scheduler ();
+
+    queue_print ("Saida gerada  ", (queue_t*) readyQueue, print_elem);
+}
---
+++ so/p3/queue.c
@@ -0,0 +1,185 @@
+// GRR20211798 Rafael Gonçalves dos Santos
+
+#include <stdio.h>
+#include "queue.h"
+
+int queue_size (queue_t *queue) {
+    queue_t *aux;
+    int tam;
+
+    // fila vazia
+    if (queue == NULL) {
+        return 0;
+    }
+    
+    aux = queue;
+    tam = 1;
+
+    // percorre toda fila até chegar no elemento que possui o next sendo o inicio da fila (ultimo elemento)
+    while (aux->next != queue) {
+        aux = aux->next;
+        tam++;
+    }
+
+    return tam;
+}
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*)) {
+    queue_t *aux = queue;
+
+    printf ("%s", name);
+    printf ("[");
+
+    // imprime o primeiro elemento
+    print_elem (queue);
+
+    // percorre toda fila até chegar no elemento que possui o next sendo o inicio da fila (ultimo elemento)
+    while (queue != NULL && aux->next != queue) {
+        printf (" ");
+
+        aux = aux->next;
+
+        print_elem (aux);
+    }
+
+    printf ("]");
+    printf ("\n");
+}
+
+int queue_append (queue_t **queue, queue_t *elem) {
+    queue_t *aux;
+    
+    // -------------------------------------------------------- //
+
+    // fila inexistente
+    if (queue == NULL) {
+        fprintf (stderr, "Fila não existe\n");
+        return 1;
+    }
+
+    // elemento inexistente
+    if (elem == NULL) {
+        fprintf (stderr, "Elemento não existe\n");
+        return 1;
+    }
+
+    // elemento pertence a outra fila
+    if (elem->prev != NULL || elem->next != NULL) {
+        fprintf (stderr, "Elemento pertence a outra fila\n");
+        return 1;
+    }
+
+    // -------------------------------------------------------- //
+
+    // fila vazia
+    if (*queue == NULL) {
+        *queue = elem;
+
+        elem->prev = elem;
+        elem->next = elem;
+
+        return 0;
+    }
+
+    // aux = ultimo elemento da fila
+    aux = (*queue)->prev;
+
+    // o prev do primeiro elemento eh o novo elemento
+    (*queue)->prev = elem;
+
+    // o next do ultimo elemento eh o novo elemento
+    aux->next = elem;
+
+    // o prev do novo elemento eh o ultimo elemento 
+    elem->prev = aux;
+
+    // o next do novo elemento eh o primeiro elemento
+    elem->next = *queue;
+
+    return 0;
+}
+
+int queue_contains (queue_t **queue, queue_t *elem) {
+    queue_t *aux;
+
+    // elemento eh o primeiro da fila
+    if (*queue == elem) {
+        return 1;
+    }
+
+    aux = *queue;
+
+    // percorre toda fila até chegar no elemento que possui o next sendo o inicio da fila (ultimo elemento)
+    while (aux->next != *queue) {
+        if (aux->next == elem) {
+            return 1;
+        }
+
+        aux = aux->next;
+    }
+
+    return 0;               
+}
+
+int queue_remove (queue_t **queue, queue_t *elem) {
+    queue_t *aux;
+
+    // -------------------------------------------------------- //
+
+    // fila inexistente
+    if (queue == NULL) {
+        fprintf (stderr, "Fila não existe\n");
+        return 1;
+    }
+
+    // elemento inexistente
+    if (elem == NULL) {
+        fprintf (stderr, "Elemento não existe\n");
+        return 1;
+    }
+
+    // fila vazia
+    if (*queue == NULL) {
+        fprintf (stderr, "Fila Vazia\n");
+        return 1;
+    }
+
+    // elemento não pertence a fila
+    if (!queue_contains (queue, elem)) {
+        fprintf (stderr, "Elemento não pertence a fila\n");
+        return 1;
+    }
+
+    // -------------------------------------------------------- //
+
+    // se for uma fila unitaria
+    if (queue_size (*queue) == 1) {
+        *queue = NULL;
+    }
+
+    // se não for uma fila unitaria
+    else {
+        // se remover o primeiro altera o inicio da fila
+        if (elem == *queue) {
+            *queue = elem->next;
+        }
+
+        // aux = prev do elemento
+        aux = elem->prev;
+
+        // next do prev do elemento eh o next do elemento
+        aux->next = elem->next;
+
+        // aux = next do elemento
+        aux = elem->next;
+        
+        // prev do next do elemento eh o prev do elemento
+        aux->prev = elem->prev;
+    }
+
+    // tira o elemento da fila
+    elem->prev = NULL;
+    elem->next = NULL;
+
+    return 0;
+}
