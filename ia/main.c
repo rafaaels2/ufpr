a28965167a9d7f7b113cab5e2f9763f0232f69e2
Rafael Santos <rafa1405rafael@gmail.com>
2024-11-30 22:50:43 -0300

    .

--- ia/main.c
+++ ia/main.c
@@ -3,10 +3,15 @@
 #include <ctype.h>
 #include <string.h>
 #include <sys/resource.h>
+#include <time.h>
 
 #define MAX_TAM 1024
+#define N_VIZINHOS 8
 
-int le_entrada (char *entrada, int *numeros) {
+
+int n_clausulas = 0; 
+
+int le_entrada (char *entrada, int *numeros, int *linhas, int *colunas) {
     int n = 0;
     
     while (n < 2 || n < (2 + numeros[0] * numeros[1])) {
@@ -27,7 +32,10 @@ int le_entrada (char *entrada, int *numeros) {
         }
     }
 
-    return n;
+    *linhas  = numeros[0];
+    *colunas = numeros[1];
+
+    return n - 2;
 }
 
 /* define o limite de GB do sistema */
@@ -42,44 +50,168 @@ int limita_memoria (int tam) {
         return 0;
     }
 
-    printf ("Memória máxima definida para %d GB\n", tam);
-
     return 1;
+}
+
+void combinar(int *vizinhos, int n, int *combinacao, int inicio, int profundidade, int tamanho, FILE *arquivo, int sinal) {
+    if (profundidade == tamanho) {
+        n_clausulas++;
+
+        for (int i = 0; i < tamanho; i++) {
+            fprintf (arquivo, "%d ", combinacao[i]);
+        }
+        fprintf (arquivo, "0\n");
+        return;
+    }
+
+    for (int i = inicio; i < n; i++) {
+        combinacao[profundidade] = vizinhos[i] * sinal;  
+        combinar (vizinhos, n, combinacao, i + 1, profundidade + 1, tamanho, arquivo, sinal);  
+    }
+}
+
+void gerar_combinacoes(int *vizinhos, int n, int tamanho, FILE *arquivo, int sinal) {
+    int *combinacao = malloc (sizeof(int) * tamanho);
+
+    if (n < tamanho) 
+        tamanho = n; 
+
+    arquivo = fopen ("clausulas.txt", "a");
+
+    combinar (vizinhos, n, combinacao, 0, 0, tamanho, arquivo, sinal);
+
+    fclose(arquivo);
+
+    free (combinacao);
 }
 
 int main () {
     limita_memoria (8);
 
-    int n;
+    int k;
+    int n, linhas, colunas;
     char *entrada = malloc (sizeof (char) * MAX_TAM);
     int *entrada_int = calloc (MAX_TAM, sizeof (float));
 
-    n = le_entrada (entrada, entrada_int);
+    double tempo;
+
+    n = le_entrada (entrada, entrada_int, &linhas, &colunas);
 
     free (entrada);
 
-    /* 
-     * posicao 0 = numero de linhas 
-     * posicao 1 = numero de colunas
-     * posicao [x] = estado da celula
-     * 
-     * celulas vao de 1 ate n, representadas por: tabuleiro_t1[x + 1]
-     * ex: celula 1 = tabuleiro_t1[2]
-     * ex: celula 8 = tabuleiro_t1[9]
-     */
+    FILE *arquivo = fopen ("clausulas.txt", "w");
+    fclose(arquivo);
+
     int *tabuleiro_t1 = malloc (sizeof (int) * n);
     for (int i = 0; i < n; i++)
-        tabuleiro_t1[i] = entrada_int[i];
+        tabuleiro_t1[i] = entrada_int[i + 2];
 
     free (entrada_int);
+
+    int **vizinhos = malloc (sizeof (int *) * n);
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            int index = i * colunas + j;
+
+            vizinhos[index] = calloc (N_VIZINHOS + 1, sizeof (int));
+
+            /* pega vizinhos */
+            int index_vizinhos = 0;
+
+            if (i > 0) { 
+                if (j > 0) 
+                    vizinhos[index][index_vizinhos++] = ((i - 1) * colunas + (j - 1)) + 1; 
+
+                vizinhos[index][index_vizinhos++] = ((i - 1) * colunas + j) + 1;               
+
+                if (j < colunas - 1) 
+                    vizinhos[index][index_vizinhos++] = ((i - 1) * colunas + (j + 1)) + 1;
+            }
+
+            if (j > 0) 
+                vizinhos[index][index_vizinhos++] = (i * colunas + (j-1)) + 1;  
+
+            if (j < colunas - 1) 
+                vizinhos[index][index_vizinhos++] = (i * colunas + (j+1)) + 1; 
+
+            if (i < linhas - 1) {  
+                if (j > 0) 
+                    vizinhos[index][index_vizinhos++] = ((i+1) * colunas + (j-1)) + 1; 
+
+                vizinhos[index][index_vizinhos++] = ((i+1) * colunas + j) + 1;  
+
+                if (j < colunas - 1) 
+                    vizinhos[index][index_vizinhos++] = ((i+1) * colunas + (j+1)) + 1;  
+            }
+
+            vizinhos[index][N_VIZINHOS] = index_vizinhos;
+
+            if (tabuleiro_t1[index] == 1) {
+                /* --------------------------------------------------------------------------- */
+
+                /* verifica se é >= 3 */
+                gerar_combinacoes (vizinhos[index], vizinhos[index][N_VIZINHOS], 6, arquivo, 1);
+
+                /* verifica se é <= 3 */
+                gerar_combinacoes (vizinhos[index], vizinhos[index][N_VIZINHOS], 4, arquivo, -1);
+
+                /* -x */
+                arquivo = fopen ("clausulas.txt", "a");
+                fprintf (arquivo, "%d 0\n", (index + 1) * -1);
+                n_clausulas++;
+                fclose (arquivo);
+
+                /* --------------------------------------------------------------------------- */
+
+                /* verifica se é >= 2 */
+                gerar_combinacoes (vizinhos[index], vizinhos[index][N_VIZINHOS], 7, arquivo, 1);
+
+                /* verifica se é <= 3 */
+                gerar_combinacoes (vizinhos[index], vizinhos[index][N_VIZINHOS], 4, arquivo, -1);
+
+                /* x */
+                arquivo = fopen ("clausulas.txt", "a");
+                fprintf (arquivo, "%d 0\n", (index + 1) * 1);
+                n_clausulas++;
+                fclose (arquivo);
+            }
+            else {
+                /* --------------------------------------------------------------------------- */
+
+                /* verifica se é <= 1 */
+                gerar_combinacoes (vizinhos[index], vizinhos[index][N_VIZINHOS], 2, arquivo, -1);
+
+                /* x */
+                arquivo = fopen ("clausulas.txt", "a");
+                fprintf (arquivo, "%d 0\n", (index + 1) * 1);
+                n_clausulas++;
+                fclose (arquivo);
+
+                /* --------------------------------------------------------------------------- */
+            }
+        }
+    }
+
+    arquivo = fopen ("clausulas.txt", "r");
+    FILE *arquivo_tmp = fopen ("tmp.txt", "w");
+
+    fprintf (arquivo_tmp, "p cnf %d %d\n", n, n_clausulas);
+
+    char buffer[MAX_TAM];
+    while (fgets (buffer, sizeof (buffer), arquivo) != NULL) 
+        fputs (buffer, arquivo_tmp);
     
-    /* inicializa tabuleiro t0 */
-    int *tabuleiro_t0 = malloc (sizeof (int) * n);
-    tabuleiro_t0[0] = tabuleiro_t1[0];
-    tabuleiro_t0[1] = tabuleiro_t1[0];
+    fclose (arquivo);
+    fclose (arquivo_tmp);
+
+    remove ("clausulas");
+    rename ("tmp.txt", "clausulas.txt");
 
     free (tabuleiro_t1);
-    free (tabuleiro_t0);
+    for (int i = 0; i < n; i++)
+        free (vizinhos[i]);
+    free (vizinhos);
 
     return 0;
 }
